ee14abb42f7f795f9d34b7dc39d414637ab43ecb
mikehollas123 <63059466+mikehollas123@users.noreply.github.com>
2022-07-01 15:30:44 -0500

    Added Chart Examples project, with LineSeries example

---
+++ src/SciChartBlazor.Demos/ChartDemos/LineRenderableSeriesChartExample.razor.cs
@@ -0,0 +1,10 @@
+namespace SciChartBlazor.Demos.ChartDemos
+{
+    public partial class LineRenderableSeriesExample
+    {
+
+        public static string __description__ = "An example using the LineRenderable series";
+
+
+    }
+}
---
+++ src/SciChartBlazor.Demos/ChartDemos/LineRenderableSeriesExample.razor.cs
@@ -0,0 +1,109 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using SciChartBlazor.Axes;
+using SciChartBlazor.DataSeries;
+using SciChartBlazor.Modifiers;
+using SciChartBlazor.RenderableSeries;
+using SciChartBlazor.Services;
+
+namespace SciChartBlazor.Demos.ChartDemos
+{
+    public partial class LineRenderableSeriesChartExample : ComponentBase
+    {
+
+        public static string __description__ = "An example using the LineRenderable series";
+        public static string __niceName__ = "LineRenderable Series";
+
+
+        public string Id { get; set; } = "C" + Guid.NewGuid().ToString();
+        protected ElementReference _chart;
+
+        SciChartBuilder _chartBuilder = default!;
+
+
+        [Inject] public IJSRuntime JSRuntime { get; set; } = default!;
+        [Inject] public ISciChartBlazorService sciChartBlazorService { get; set; } = default!;
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                _chartBuilder = new SciChartBuilder(_chart, JSRuntime, sciChartBlazorService);
+                await _chartBuilder.CreateChart();
+                await AddModifiers();
+                await CreateAxis();
+                await LoadData();
+
+
+            }
+
+            await base.OnAfterRenderAsync(firstRender);
+   
+        }
+        private async Task LoadData()
+        {
+            double[] x = new double[1000];
+            double[] y = new double[1000];
+
+            for (int i =0; i < 1000; i++)
+            {
+                x[i] = (double)i;
+                y[i] = Math.Sin(i * 2);
+            }
+
+            XyDataSeries<double, double> dataSeries = new(x, y) { DataSeriesName = "Data", ContainsNaN = false, DataIsSortedInX = true };
+            FastLineRenderableSeries<double, double> series = new(dataSeries)
+            {
+                Stroke = "black",
+                StrokeThickness = 3,
+            };
+
+            await _chartBuilder.AddSeries(series);
+        }
+
+        private async Task AddModifiers()
+        {
+            List<ModifierBase> modifiers = new()
+            {
+                new SciChartBlazor.Modifiers.RubberBandXyZoomModifier
+                {
+                    IsAnimated = true,
+                    AnimationDuration = 400,
+                    Fill = "#FFFFFF33",
+                    Stroke = "#4E2A84",
+                    StrokeThickness = 1,
+                    XyDirection = XyDirection.XDirection
+                },
+                new SciChartBlazor.Modifiers.ZoomExtentsModifier
+                {
+                    IsAnimated = true,
+                    AnimationDuration = 400
+                },
+                new MouseWheelZoomModifier(),
+                new ZoomPanModifier() { ExecuteOn = ExecuteOn.MouseMiddleButton }
+            };
+
+            await _chartBuilder.AddModifiers(modifiers);
+        }
+
+        private async Task CreateAxis()
+        {
+            var XAxis = new NumericAxis()
+            {
+                AxisTitle = "X",
+                GrowBy = new SciChartNumberRange(0.05, 0.05)
+            };
+            var YAxis = new NumericAxis()
+            {
+                AxisTitle = "Y",
+                AxisAlignment = AxisAlignment.Left,
+                AutoRange = AutoRange.Always,
+            };
+
+            await _chartBuilder.AddAxis(XAxis, AxisType.X);
+            await _chartBuilder.AddAxis(YAxis, AxisType.Y);
+        }
+
+
+    }
+}
