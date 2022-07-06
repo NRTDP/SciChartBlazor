f9fac6aa784a394bf4091efa7f2456ea5ae31809
mikehollas123 <63059466+mikehollas123@users.noreply.github.com>
2022-07-06 11:31:33 -0500

    playing with secrets

--- src/SciChartBlazor.Demos/Program.cs
+++ src/SciChartBlazor.Demos/Program.cs
@@ -9,6 +9,11 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Configuration.SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+    .AddUserSecrets<App>()
+    .AddJsonFile("/wwwroot/appsettings.json",true)
+    .Build();
+
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
