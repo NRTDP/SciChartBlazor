bdc7056e9856a47d18f389eb7b16d1e102a6fb15
Jarek Jannait <jjannait@haasf1team.com>
2023-12-14 10:23:42 +0100

    Add second series that it similar to first, but with StrokeDashArray to demo

--- src/SciChartBlazor.Shared/ChartDemos/LineRenderableSeriesChartExample.razor.cs
+++ src/SciChartBlazor.Shared/ChartDemos/LineRenderableSeriesChartExample.razor.cs
@@ -46,6 +46,7 @@ namespace SciChartBlazor.Shared.ChartDemos
                 await AddModifiers();
                 await CreateAxis();
                 await LoadData();
+                await LoadAlternativeData();
             }
 
             await base.OnAfterRenderAsync(firstRender);
@@ -74,6 +75,27 @@ namespace SciChartBlazor.Shared.ChartDemos
                    Intensity = 1,
                     Offset = new Point(5, 5)
                  }
+            };
+
+            await _chartBuilder.AddSeries(series);
+        }
+        
+        private async Task LoadAlternativeData()
+        {
+            var x = new double[1000];
+            var y = new double[1000];
+
+            for (var i = 0; i < 1000; i++)
+            {
+                x[i] = i+10;
+                y[i] = Math.Sin(i * 0.2) * 100;
+            }
+
+            XyDataSeries<double, double> dataSeries = new(x, y) { DataSeriesName = "Alternative Data", ContainsNaN = false, DataIsSortedInX = true };
+            FastLineRenderableSeries<double, double> series = new(dataSeries)
+            {
+                StrokeThickness = 1,
+                StrokeDashArray = new double[]{10, 3}
             };
 
             await _chartBuilder.AddSeries(series);
